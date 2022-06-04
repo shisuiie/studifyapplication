f4b3dee86a7fe47324edcb1439bdbd1202dabb1a
shisuiie <84363757+shisuiie@users.noreply.github.com>
2022-06-04 16:43:29 +1200

    Log in Screen completed code/with Firebase authentication

---
+++ app/src/main/java/com/example/studifyapplication/AppCompatActvity.java
@@ -0,0 +1,4 @@
+package com.example.studifyapplication;
+
+public class AppCompatActvity {
+}
---
+++ app/src/main/java/com/example/studifyapplication/SplashActivity.java
@@ -0,0 +1,55 @@
+package com.example.studifyapplication;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_SCREEN_TIMEOUT = 2000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.splash_activity);
+
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setInterpolator(new AccelerateInterpolator());
+        fadeOut.setStartOffset(500);
+        fadeOut.setDuration(1800);
+        ImageView image;
+        image = findViewById(R.id.imageView4);
+
+        image.setAnimation(fadeOut);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+
+            private void startActivity(Intent intent) {
+            }
+        }, SPLASH_SCREEN_TIMEOUT);
+
+
+
+    }
+}
